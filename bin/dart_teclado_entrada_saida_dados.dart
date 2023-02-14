fb11eafaac1088cc3e0914864b6f65ba0655b328
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-02-14 16:21:11 -0300

    Commit 1

---
+++ bin/dart_teclado_entrada_saida_dados.dart
@@ -0,0 +1,5 @@
+import 'package:dart_teclado_entrada_saida_dados/dart_teclado_entrada_saida_dados.dart' as dart_teclado_entrada_saida_dados;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_teclado_entrada_saida_dados.calculate()}!');
+}
---
+++ test/dart_teclado_entrada_saida_dados_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_teclado_entrada_saida_dados/dart_teclado_entrada_saida_dados.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
